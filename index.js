dda363ed91a833c5da216c0d25146e71598ebdbd
Kelvin <kmwendwa476@gmail.com>
2022-06-23 08:43:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData (name , email){
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+        body: JSON.stringify({name, email}),
+    })
+    .then( (response)=>response.json())
+    .then(function (data) {
+        return document.body.innerHTML = data["id"]
+        })
+        .catch(function (error) {
+        alert(error.message);
+        document.body.innerHTML = error.message;
+      })
+    }
